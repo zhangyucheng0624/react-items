491a90dd4c7730e53ca4988b069c741f6e181d5e
zhangyc <az1578988641@163.com>
2019-06-23 23:42:05 +0800

    完善left-nav部分功能

--- src/component/left-nav/index.jsx
+++ src/component/left-nav/index.jsx
@@ -1,69 +1,50 @@
 import React from "react";
 import { Menu, Icon } from 'antd';
+import {Link,withRouter} from "react-router-dom";
 import Logo from "../../assets/imags/logo.png";
-import "./index.less"
+import "./index.less";
+import menuList from "../../config/menuConfig"
 const { SubMenu } = Menu;
-export default class LeftNav extends React.Component{
-  render() {
-    return  <div>
-      <div className="nav-logo" >
-        <img src={Logo} alt="Logo"/>
-        <span>硅谷后台</span>
-      </div>
-      <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-        <Menu.Item key="1">
-          <Icon type="home" />
-          <span>首页</span>
-        </Menu.Item>
-        <SubMenu
-          key="sub1"
+const Item = Menu.Item;
+class LeftNav extends React.Component{
+  createMenu = (menu) => {
+    return <Item key={menu.key}>
+      <Icon type={menu.Icon} />
+      <span>{menu.title}</span>
+    </Item>
+  };
+  componentWillMount() {
+    this.menuALL = menuList.map((menu)=> {
+      if (menu.children) {
+        return <SubMenu
+          key={menu.key}
           title={
             <span>
-      <Icon type="appstore" />
-      <span>商品</span>
+      <Icon type={menu.Icon} />
+      <span>{menu.title}</span>
       </span>
           }
         >
-          <Menu.Item key="">
-            <Icon type="unordered-list" />
-            <span>品类管理</span>
-          </Menu.Item>
-          <Menu.Item key="">
-            <Icon type="tool" />
-            <span>商品管理</span>
-          </Menu.Item>
-        </SubMenu>
-        <Menu.Item key="">
-          <Icon type="user" />
-          <span>用户管理</span>
-        </Menu.Item>
-        <Menu.Item key="">
-          <Icon type="safety-certificate" />
-          <span>权限管理</span>
-        </Menu.Item>
-        <SubMenu
-          key="sub2"
-          title={
-        <span>
-          <Icon type="team" />
-          <span>图形图表</span>
-        </span>
+          {
+            menu.children.map((item) => this.createMenu(item))
           }
-        >
-          <Menu.Item key="">
-            <Icon type="bar-chart" />
-            <span>柱形图</span>
-          </Menu.Item>
-          <Menu.Item key="">
-            <Icon type="line-chart" />
-            <span>折线图</span>
-          </Menu.Item>
-          <Menu.Item key="">
-            <Icon type="pie-chart" />
-            <span>饼图</span>
-          </Menu.Item>
         </SubMenu>
+      }else {
+        return this.createMenu(menu);
+      }
+    })
+  }
+
+  render() {
+    return  <div>
+      <Link className="nav-logo" to="/home">
+        <img src={Logo} alt="Logo"/>
+        <span>硅谷后台</span>
+      </Link>
+      <Menu theme="dark" defaultSelectedKeys={[this.props.location.pathname]} mode="inline">
+        {this.menuALL}
       </Menu>
     </div>
   }
 }
+export default withRouter(LeftNav);
